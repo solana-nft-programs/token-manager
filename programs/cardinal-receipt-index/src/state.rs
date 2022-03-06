edb4d8d097365502dfe7b76f393f72f50c3eb746
jpbogle <jpbogle22@gmail.com>
2022-03-06 15:52:35 -0800

    CRD-25: Remove receipt index

--- programs/cardinal-receipt-index/src/state.rs
+++
@@ -1,9 +0,0 @@
-use anchor_lang::prelude::*;
-
-pub const RECEIPT_MARKER_SEED: &str = "receipt-marker";
-pub const RECEIPT_MARKER_SIZE: usize = 8 + std::mem::size_of::<ReceiptMarker>(); 
-#[account]
-pub struct ReceiptMarker {
-    pub bump: u8,
-    pub receipt_manager: Pubkey
-}
--- src/programs/receiptIndex/index.ts
+++
@@ -1,5 +0,0 @@
-export * as accounts from "./accounts";
-export * from "./api";
-export * from "./constants";
-export * as instruction from "./instruction";
-export * from "./pda";

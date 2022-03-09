39f7986b4b2c3ea4e13e0293748c529c41e2a252
jpbogle <jpbogle22@gmail.com>
2022-03-08 17:49:37 -0800

    Bump version

--- src/idl/cardinal_paid_claim_approver.ts
+++ src/idl/cardinal_paid_claim_approver.ts
@@ -1,5 +1,5 @@
 export type CardinalPaidClaimApprover = {
-  version: "0.2.11";
+  version: "0.2.15";
   name: "cardinal_paid_claim_approver";
   instructions: [
     {
@@ -156,7 +156,7 @@ export type CardinalPaidClaimApprover = {
 };
 
 export const IDL: CardinalPaidClaimApprover = {
-  version: "0.2.11",
+  version: "0.2.15",
   name: "cardinal_paid_claim_approver",
   instructions: [
     {

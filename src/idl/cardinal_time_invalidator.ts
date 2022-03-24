bd130aafad3d5e68a897b584962bb72485c8f61d
jpbogle <jpbogle22@gmail.com>
2022-03-23 17:34:40 -0700

    Bump version

--- src/idl/cardinal_time_invalidator.ts
+++ src/idl/cardinal_time_invalidator.ts
@@ -1,5 +1,5 @@
 export type CardinalTimeInvalidator = {
-  version: "0.2.21";
+  version: "0.2.22";
   name: "cardinal_time_invalidator";
   instructions: [
     {
@@ -347,7 +347,7 @@ export type CardinalTimeInvalidator = {
 };
 
 export const IDL: CardinalTimeInvalidator = {
-  version: "0.2.21",
+  version: "0.2.22",
   name: "cardinal_time_invalidator",
   instructions: [
     {

ea37a8ab45385cee9791a5b9cc6bbbaf67293aa8
Avinash Jain <avinashkalpitjain@gmail.com>
2022-05-06 12:03:51 -0700

    bumped token manager version

--- src/idl/cardinal_token_manager.ts
+++ src/idl/cardinal_token_manager.ts
@@ -1,5 +1,5 @@
 export type CardinalTokenManager = {
-  version: "1.3.8";
+  version: "1.3.9";
   name: "cardinal_token_manager";
   instructions: [
     {
@@ -978,7 +978,7 @@ export type CardinalTokenManager = {
 };
 
 export const IDL: CardinalTokenManager = {
-  version: "1.3.8",
+  version: "1.3.9",
   name: "cardinal_token_manager",
   instructions: [
     {

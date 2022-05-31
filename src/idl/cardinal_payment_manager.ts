f764a2c696d11e256fdc04e575184a1d9f829932
Giannis Chatziveroglou <gchatz@mit.edu>
2022-05-31 18:08:06 -0400

    bump version

--- src/idl/cardinal_payment_manager.ts
+++ src/idl/cardinal_payment_manager.ts
@@ -1,5 +1,5 @@
 export type CardinalPaymentManager = {
-  version: "1.4.0";
+  version: "1.4.2";
   name: "cardinal_payment_manager";
   instructions: [
     {
@@ -173,7 +173,7 @@ export type CardinalPaymentManager = {
 };
 
 export const IDL: CardinalPaymentManager = {
-  version: "1.4.0",
+  version: "1.4.2",
   name: "cardinal_payment_manager",
   instructions: [
     {

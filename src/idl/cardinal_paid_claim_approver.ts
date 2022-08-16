ca4e16550892793ba1bb9a7561db72e72d3d4d39
jpbogle <jpbogle22@gmail.com>
2022-08-16 19:44:43 -0400

    Bump version

--- src/idl/cardinal_paid_claim_approver.ts
+++ src/idl/cardinal_paid_claim_approver.ts
@@ -1,5 +1,5 @@
 export type CardinalPaidClaimApprover = {
-  version: "1.5.0";
+  version: "1.5.12";
   name: "cardinal_paid_claim_approver";
   instructions: [
     {
@@ -253,7 +253,7 @@ export type CardinalPaidClaimApprover = {
 };
 
 export const IDL: CardinalPaidClaimApprover = {
-  version: "1.5.0",
+  version: "1.5.12",
   name: "cardinal_paid_claim_approver",
   instructions: [
     {
--- src/idl/cardinal_token_manager.ts
+++ src/idl/cardinal_token_manager.ts
@@ -1,5 +1,5 @@
 export type CardinalTokenManager = {
-  version: "1.5.0";
+  version: "1.5.12";
   name: "cardinal_token_manager";
   instructions: [
     {
@@ -978,7 +978,7 @@ export type CardinalTokenManager = {
 };
 
 export const IDL: CardinalTokenManager = {
-  version: "1.5.0",
+  version: "1.5.12",
   name: "cardinal_token_manager",
   instructions: [
     {

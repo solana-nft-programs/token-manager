efe8bf280c9102250b027b462051ec9743bddf93
Giannis Chatziveroglou <gchatz@mit.edu>
2022-09-22 12:24:50 -0700

    bump version

--- src/idl/cardinal_token_manager.ts
+++ src/idl/cardinal_token_manager.ts
@@ -1,5 +1,5 @@
 export type CardinalTokenManager = {
-  version: "1.5.19";
+  version: "1.5.20";
   name: "cardinal_token_manager";
   instructions: [
     {
@@ -978,7 +978,7 @@ export type CardinalTokenManager = {
 };
 
 export const IDL: CardinalTokenManager = {
-  version: "1.5.19",
+  version: "1.5.20",
   name: "cardinal_token_manager",
   instructions: [
     {

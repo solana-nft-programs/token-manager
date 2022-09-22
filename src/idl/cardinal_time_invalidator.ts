220828ec4dd179dea18ca6e9ba99f743a330044a
Giannis Chatziveroglou <gchatz@mit.edu>
2022-09-22 12:17:36 -0700

    bump version

--- src/idl/cardinal_time_invalidator.ts
+++ src/idl/cardinal_time_invalidator.ts
@@ -1,5 +1,5 @@
 export type CardinalTimeInvalidator = {
-  version: "1.5.18";
+  version: "1.5.19";
   name: "cardinal_time_invalidator";
   instructions: [
     {
@@ -407,7 +407,7 @@ export type CardinalTimeInvalidator = {
 };
 
 export const IDL: CardinalTimeInvalidator = {
-  version: "1.5.18",
+  version: "1.5.19",
   name: "cardinal_time_invalidator",
   instructions: [
     {
--- src/idl/cardinal_use_invalidator.ts
+++ src/idl/cardinal_use_invalidator.ts
@@ -1,5 +1,5 @@
 export type CardinalUseInvalidator = {
-  version: "1.5.18";
+  version: "1.5.19";
   name: "cardinal_use_invalidator";
   instructions: [
     {
@@ -410,7 +410,7 @@ export type CardinalUseInvalidator = {
 };
 
 export const IDL: CardinalUseInvalidator = {
-  version: "1.5.18",
+  version: "1.5.19",
   name: "cardinal_use_invalidator",
   instructions: [
     {

f8b948234b23af821a4bb1d54bb9434582c66a29
jpbogle <jpbogle22@gmail.com>
2022-10-04 19:44:35 -0400

    Should time invalidate additional checks

--- src/programs/timeInvalidator/utils.ts
+++ src/programs/timeInvalidator/utils.ts
@@ -10,20 +10,25 @@ export const shouldTimeInvalidate = (
   timeInvalidatorData: AccountData<TimeInvalidatorData>,
   UTCNow: number = Date.now() / 1000
 ): boolean => {
-  return Boolean(
+  const invalidators = tokenManagerData.parsed.invalidators.map((i) =>
+    i.toString()
+  );
+  return (
+    invalidators.includes(timeInvalidatorData.pubkey.toString()) &&
     tokenManagerData?.parsed.state !== TokenManagerState.Invalidated &&
-      ((timeInvalidatorData.parsed.maxExpiration &&
-        new BN(UTCNow).gte(timeInvalidatorData.parsed.maxExpiration)) ||
-        (timeInvalidatorData.parsed.expiration &&
-          tokenManagerData.parsed.state === TokenManagerState.Claimed &&
-          new BN(UTCNow).gte(timeInvalidatorData.parsed.expiration)) ||
-        (!timeInvalidatorData.parsed.expiration &&
-          tokenManagerData.parsed.state === TokenManagerState.Claimed &&
-          timeInvalidatorData.parsed.durationSeconds &&
-          new BN(UTCNow).gte(
-            tokenManagerData.parsed.stateChangedAt.add(
-              timeInvalidatorData.parsed.durationSeconds
-            )
-          )))
+    tokenManagerData?.parsed.state !== TokenManagerState.Initialized &&
+    ((timeInvalidatorData.parsed.maxExpiration &&
+      new BN(UTCNow).gte(timeInvalidatorData.parsed.maxExpiration)) ||
+      (timeInvalidatorData.parsed.expiration &&
+        tokenManagerData.parsed.state === TokenManagerState.Claimed &&
+        new BN(UTCNow).gte(timeInvalidatorData.parsed.expiration)) ||
+      (!timeInvalidatorData.parsed.expiration &&
+        tokenManagerData.parsed.state === TokenManagerState.Claimed &&
+        !!timeInvalidatorData.parsed.durationSeconds &&
+        new BN(UTCNow).gte(
+          tokenManagerData.parsed.stateChangedAt.add(
+            timeInvalidatorData.parsed.durationSeconds
+          )
+        )))
   );
 };

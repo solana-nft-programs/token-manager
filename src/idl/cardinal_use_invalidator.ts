d0eafae5a6d7b610acf88892a02d862922bd4d13
Giannis Chatziveroglou <gchatz@mit.edu>
2022-10-20 09:49:53 -0700

    bump version

--- src/idl/cardinal_use_invalidator.ts
+++ src/idl/cardinal_use_invalidator.ts
@@ -1,5 +1,5 @@
 export type CardinalUseInvalidator = {
-  version: "1.7.3";
+  version: "1.7.4";
   name: "cardinal_use_invalidator";
   instructions: [
     {
@@ -410,7 +410,7 @@ export type CardinalUseInvalidator = {
 };
 
 export const IDL: CardinalUseInvalidator = {
-  version: "1.7.3",
+  version: "1.7.4",
   name: "cardinal_use_invalidator",
   instructions: [
     {

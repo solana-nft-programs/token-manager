dbaf2c48c74f256a1ded93e149c1a45928da8ada
jpbogle <jpbogle22@gmail.com>
2022-12-15 16:36:15 -0500

    generate-idls script

--- generate-idls.sh
+++ generate-idls.sh
@@ -8,7 +8,7 @@ mkdir -p src/idl/
 for PROGRAM in $(find programs/ -maxdepth 3 -name lib.rs); do
     PROGRAM_NAME=$(dirname $PROGRAM | xargs dirname | xargs basename | tr '-' '_')
     echo "Parsing IDL for $PROGRAM_NAME"
-    anchor idl parse --file $PROGRAM --out-ts src/idl/$PROGRAM_NAME.ts || {
+    anchor idl parse --file $PROGRAM --out-ts src/idl/$PROGRAM_NAME.ts --out src/idl/"$PROGRAM_NAME"_idl.json || {
         echo "Could not parse IDL"
         exit 1
     }

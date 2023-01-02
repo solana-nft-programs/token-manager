b4231a31a3459ab6bf84e361132489d5826cd81c
Giannis Chatziveroglou <gchatz@mit.edu>
2023-01-02 16:45:52 +0000

    API directory updates (#369)
    * api directory updates
    
    * bump node version
    
    * swap out genesys go rpc

--- api/common/connection.ts
+++ api/common/connection.ts
@@ -3,12 +3,16 @@ import { Connection } from "@solana/web3.js";
 const networkURLs: { [key: string]: { primary: string; secondary?: string } } =
   {
     ["mainnet-beta"]: {
-      primary: process.env.MAINNET_PRIMARY || "https://ssc-dao.genesysgo.net/",
-      secondary: "https://ssc-dao.genesysgo.net/",
+      primary:
+        process.env.MAINNET_PRIMARY ||
+        "https://explorer-api.mainnet-beta.solana.com/",
+      secondary: "https://explorer-api.mainnet-beta.solana.com/",
     },
     mainnet: {
-      primary: process.env.MAINNET_PRIMARY || "https://ssc-dao.genesysgo.net/",
-      secondary: "https://ssc-dao.genesysgo.net/",
+      primary:
+        process.env.MAINNET_PRIMARY ||
+        "https://explorer-api.mainnet-beta.solana.com/",
+      secondary: "https://explorer-api.mainnet-beta.solana.com/",
     },
     devnet: { primary: "https://api.devnet.solana.com/" },
     testnet: { primary: "https://api.testnet.solana.com/" },

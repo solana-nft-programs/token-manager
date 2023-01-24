6a65d9bf7ae2549ea7ccfd3a39d05af76270e8df
jpbogle <jpbogle22@gmail.com>
2023-01-24 17:32:58 -0500

    Bump anchor version

--- programs/cardinal-transfer-authority/src/instructions/transfer/accept_transfer.rs
+++ programs/cardinal-transfer-authority/src/instructions/transfer/accept_transfer.rs
@@ -83,7 +83,6 @@ pub fn handler<'key, 'accounts, 'remaining, 'info>(ctx: Context<'key, 'accounts,
             mint: ctx.accounts.mint.to_account_info(),
             system_program: ctx.accounts.system_program.to_account_info(),
             token_program: ctx.accounts.token_program.to_account_info(),
-            rent: ctx.accounts.rent.to_account_info(),
         };
         let cpi_program = ctx.accounts.token_program.to_account_info();
         let cpi_context = CpiContext::new(cpi_program, cpi_accounts);

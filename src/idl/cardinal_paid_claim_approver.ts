29ae01d9731905e1e7d71a661ac30ca587b43bf1
Giannis Chatziveroglou <gchatz@mit.edu>
2023-02-14 14:24:44 -0800

    bump version

--- src/idl/cardinal_paid_claim_approver.ts
+++ src/idl/cardinal_paid_claim_approver.ts
@@ -1,5 +1,5 @@
 export type CardinalPaidClaimApprover = {
-  version: "4.2.1";
+  version: "4.3.0";
   name: "cardinal_paid_claim_approver";
   instructions: [
     {
@@ -253,7 +253,7 @@ export type CardinalPaidClaimApprover = {
 };
 
 export const IDL: CardinalPaidClaimApprover = {
-  version: "4.2.1",
+  version: "4.3.0",
   name: "cardinal_paid_claim_approver",
   instructions: [
     {
--- src/idl/cardinal_time_invalidator.ts
+++ src/idl/cardinal_time_invalidator.ts
@@ -1,5 +1,5 @@
 export type CardinalTimeInvalidator = {
-  version: "4.2.1";
+  version: "4.3.0";
   name: "cardinal_time_invalidator";
   instructions: [
     {
@@ -452,7 +452,7 @@ export type CardinalTimeInvalidator = {
 };
 
 export const IDL: CardinalTimeInvalidator = {
-  version: "4.2.1",
+  version: "4.3.0",
   name: "cardinal_time_invalidator",
   instructions: [
     {
